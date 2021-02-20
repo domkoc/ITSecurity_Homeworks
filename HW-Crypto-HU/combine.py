de74343a31386615ea17ce8cdf99c5cd25be8f81
Dominik Kocka <kocka98@yahoo.com>
2021-02-20 16:57:17 +0100

    first commit

---
+++ HW-Crypto-HU/combine.py
@@ -0,0 +1,15 @@
+oszlop1 = ['I', 'S', 'F', 'K', 'P']
+oszlop2 = ['T']
+oszlop3 = ['C', 'G', 'Y', 'M']
+oszlop4 = ['H', 'U', 'A']
+oszlop5 = ['B', 'R', 'Z']
+
+megoldas = []
+
+for betu1 in oszlop1:
+    for betu2 in oszlop2:
+        for betu3 in oszlop3:
+            for betu4 in oszlop4:
+                for betu5 in oszlop5:
+                    print(f'{betu1}{betu2}{betu3}{betu4}{betu5}')
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# ITSecurity_Homeworks
